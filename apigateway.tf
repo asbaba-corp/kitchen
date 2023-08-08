fbb8f38f3dbe892cc67f4d98244d4fabd31793ba
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-08-07 21:34:37 -0300

    Update apigateway.tf

--- apigateway.tf
+++ apigateway.tf
@@ -13,7 +13,13 @@ terraform {
     }
   }
 }
-
+provider "aws" {
+  region = "us-east-1"
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
 module "api_gateway" {
   source = "terraform-aws-modules/apigateway-v2/aws"
 
