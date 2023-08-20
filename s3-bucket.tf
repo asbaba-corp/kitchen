d1f3a1d70819d74f4736059d754cbd16be100df4
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-08-19 23:24:32 -0300

    add s3-bucket frontend

---
+++ s3-bucket.tf
@@ -0,0 +1,29 @@
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.0.0" 
+
+  bucket = "balcony-frontend"
+  acl    = "public-read"
+
+  website = {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+   block_public_acls       = false
+   block_public_policy     = false
+   restrict_public_buckets = false
+   policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::balcony-frontend/*"
+    }
+  ]
+}
+POLICY
+}
